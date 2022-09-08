008027ca383ff6aeb7464aeee93b8cfc691f65f9
Happiness Adebayo <adebayohappiness@gmail.com>
2022-09-08 16:32:06 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	char Mytxt[] = "with proper grammar, but the outcome is a piece of art,"
-
-	printf("%s", Mytxt);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
