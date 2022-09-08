79bbf595264c99c122033d95beb15d0d714f4524
Happiness Adebayo <adebayohappiness@gmail.com>
2022-09-08 16:19:05 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'using puts to print a string'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mytxt[] = "with proper grammar, but the outcome is a piece of art,"
+
+	printf("%s", Mytxt);
+	return (0);
+}
