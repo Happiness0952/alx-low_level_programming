2453202b0fbce364b5d5cae2c27ae7a09911d677
Happiness Adebayo <adebayohappiness@gmail.com>
2022-09-08 14:41:51 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+/**
+ *
+ *  * main - Entry point 
+ *
+ *   * print - Programming is like building a multilingual puzzle
+ *
+ *    *Return 0
+ *
+ *     
+ */
+
+ #include<stdio.h>
+
+ int main ()
+{
+
+ char Mytxt[] = "Programming is like building a multilingual puzzle" put(Mytxt);
+
+		          return 0;
+
+}
