b89637c9ca9b933c26ece9cc6a4b1d1f0f39739f
Happiness Adebayo <adebayohappiness@gmail.com>
2022-09-09 19:03:40 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char alph;
+
+	num = '0';
+	alph = 'a';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (alph <= 'f')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
