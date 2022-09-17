9ca66cb826d9bddb2067087a39cf08b9c4c23477
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-17 10:56:47 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *  * main - Print out all the numbers between one to 100
+ *   * Constraints - Print fizz for multiples of 3, Buzz for multiples of 5
+ *    * and fizz buzz for multiples of 5 and 3
+ *     * Return: void
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
