c8651b922e12d44235746cda8cf33a4be1ba3fed
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-20 08:47:19 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * swap_int - check the code
+ *   * @a: pointer variable.
+ *    * @b: pointer variable.
+ *     * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
