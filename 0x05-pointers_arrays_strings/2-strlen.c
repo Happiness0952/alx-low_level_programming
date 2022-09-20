d97890d4bffde8a68c988a38ff5ed11392891dd9
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-20 08:54:47 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ *  * _strlen - check the code
+ *   * @s: pointer variable.
+ *    * Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; s++)
+		len++;
+	return (len);
+}
