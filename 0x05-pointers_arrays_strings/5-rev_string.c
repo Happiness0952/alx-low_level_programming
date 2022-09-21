bc479405c88e873a13397618e91ce2a2521cf92e
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-21 12:47:59 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * rev_string - check the code
+ *   * @s: pointer declared.
+ *    * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+	char ch;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	for (b = 0; b < a / 2; b++)
+	{
+		ch = s[b];
+		s[b] = s[a - 1 - b];
+		s[a - 1 - b] = ch;
+	}
+
+}
