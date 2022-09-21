200a25f7a2d00fd0a51a5b893aa76593adfccc84
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-21 13:38:39 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,13 @@ void puts_half(char *str)
 
 	for (l = 0; str[l] != '\0'; ++l)
 		;
+
 	if (l % 2 == 0)
-	{ 
-		for (n = 1 / 2; str[n] != '\0'; ++n)
+	{
+		for (n = l / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	} else
+	
 	{
 		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
 			_putchar(str[n]);
