de1f815cb9911f6d6a620b7b33357295e6255473
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-29 14:10:18 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * factorial - factorial of a given number
+ * @n: variable
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
