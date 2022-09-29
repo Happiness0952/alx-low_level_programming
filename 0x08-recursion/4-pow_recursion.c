e0df8717e39cc3031c87960042a44828fd7a003a
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-29 14:54:28 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - _print a variable that has power
+ * @x, @y: varialbes
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (x == 0 || x == 1 || y == 0)
+		return (1);
+	return (_pow_recursion(x, y-1) *x);
+}
