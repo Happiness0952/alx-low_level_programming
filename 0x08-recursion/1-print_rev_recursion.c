cfcafa75d0679894d49e1ccaf8316ce89830a776
Happiness0952 <adebayohappiness01@gmail.com>
2022-09-29 13:43:50 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_print_rev_recursion - check the code
+* @s: variable.
+* Return: Always 0.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
